a01d51cec7c349fdc15d616df3dd53700c022bd4
Asavari Hangal <165581544+Emit-009@users.noreply.github.com>
2024-04-22 01:49:27 +0530

    Update layer_normalisation_inference.sv

--- layer_normalisation/layer_normalisation_inference.sv
+++ layer_normalisation/layer_normalisation_inference.sv
@@ -1,4 +1,5 @@
 module layer_normalization_inference (
+    input clk, // Clock input
     input [7:0] input_tensor [0:2], // Input tensor (3x8) represented as 1D array
     output reg [15:0] output_tensor [0:2] // Normalized output tensor (3x16)
 );
@@ -15,13 +16,15 @@ integer i;
 reg [15:0] normalized_feature;
 
 // Layer Normalization operation
-always @* begin
-    for (i = 0; i < 3; i = i + 1) begin
+always @(posedge clk) 
+begin
+    for (i = 0; i < 3; i = i + 1) 
+    begin
         // Normalize the input tensor using the pre-calculated parameters
         normalized_feature = (input_tensor[i] - MEAN[i]) * GAMMA_OVER_SQRT_VARIANCE[i] + BETA[i];
         
         // Store the normalized feature in the output tensor
-        output_tensor[i] = normalized_feature;
+        output_tensor[i] <= normalized_feature;
     end
 end
 
