0a75cc34e3f7e246a52651d69d185f1f0017c706
Soumya Kulhalli <83526493+Soumya2754@users.noreply.github.com>
2024-05-20 22:14:55 +0530

    Update maxValue.v

--- SoftMax Layer/maxValue.v
+++ SoftMax Layer/maxValue.v
@@ -9,14 +9,14 @@ module maxValue (
 reg [15:0] max = 16'b0000000000000000;
 
 always @* begin
-    if (in1[14:11] > in2[14:11])
+    if (in1[14:10] > in2[14:10])
         max = in1;
-    else if (in1[14:11] < in2[14:11])
+    else if (in1[14:10] < in2[14:10])
         max = in2;
-    else if (in1[14:11] == in2[14:11]) begin
-        if (in1[10:0] > in2[10:0])
+    else if (in1[14:10] == in2[14:10]) begin
+        if (in1[9:0] > in2[9:0])
             max = in1;
-        else if (in1[10:0] < in2[10:0])
+        else if (in1[9:0] < in2[9:0])
             max = in2;
         else
             max = in1;
