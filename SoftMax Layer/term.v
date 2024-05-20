6aad9fc44aad878d1ee03f0a0efa619a81bb5025
Soumya Kulhalli <83526493+Soumya2754@users.noreply.github.com>
2024-05-20 16:16:51 +0530

    Update term.v

--- SoftMax Layer/term.v
+++ SoftMax Layer/term.v
@@ -1,24 +1,32 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-//By: Basant Loay Abdalla
-//////////////////////////////////////////////////////////////////////////////////
-// module term multiply input number by its powered then multiply by constant 
-module term(originalNum,poweredNum,reciporical,outPower,outTerm);
-input [31:0] originalNum;
-input [31:0] poweredNum;
-input [31:0] reciporical;
-output [31:0] outTerm;
-output [31:0] outPower;
-wire [31:0] t;
-mul mulPower(.flp_a(originalNum),
-               .flp_b(poweredNum),
-               .sign(outPower[31]),
-               .exponent(outPower[30:23]),
-               .prod(outPower[22:0]));
-assign t=outPower; 
-mul mulTerm(.flp_a(t),
-            .flp_b(reciporical),
-            .sign(outTerm[31]),
-            .exponent(outTerm[30:23]),
-            .prod(outTerm[22:0]));               
+
+module term (
+    input [15:0] originalNum,
+    input [15:0] poweredNum,
+    input [15:0] reciporical,
+    output reg [15:0] outPower,
+    output reg [15:0] outTerm
+);
+
+wire [15:0] t;
+
+mul mulPower(
+    .flp_a(originalNum),
+    .flp_b(poweredNum),
+    .sign(outPower[15]),
+    .exponent(outPower[14:11]),
+    .prod(outPower[10:0])
+);
+
+assign t = outPower;
+
+mul mulTerm(
+    .flp_a(t),
+    .flp_b(reciporical),
+    .sign(outTerm[15]),
+    .exponent(outTerm[14:11]),
+    .prod(outTerm[10:0])
+);
+
 endmodule
+
