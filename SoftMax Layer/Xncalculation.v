e9d55de326bca966f79c3fdf56aa3f4533cef9e5
Soumya Kulhalli <83526493+Soumya2754@users.noreply.github.com>
2024-05-20 16:11:59 +0530

    Update Xncalculation.v

--- SoftMax Layer/Xncalculation.v
+++ SoftMax Layer/Xncalculation.v
@@ -1,35 +1,37 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-//By: Basant Loay Abdalla
-//////////////////////////////////////////////////////////////////////////////////
-// THIS MODULE FOR CALCULATING Xn value 
-// as X = X * (2-D*X)
-/////////////////////////////////////////////////////////////////////////////////
-module Xncalculation(D,X,Xn);
-localparam [31:0] two= 32'b01000000000000000000000000000000; // value of 2
-input[31:0]D;
-input[31:0]X;
-output[31:0]Xn;
-wire [31:0] term1;
-wire [31:0] term2;
-wire [31:0] term3;
-wire [31:0] x1;
-assign x1=X;
-mul mul1(.flp_a(D),
-         .flp_b(X),
-         .sign(term1[31]),
-         .exponent(term1[30:23]),
-         .prod(term1[22:0]));
-         
-assign term2[31]= !term1[31];
-assign term2[30:0]= term1[30:0];
 
-add addition3(.A_FP(two),
-                .B_FP(term2),
-                .out(term3));
-mul mul2(.flp_a(x1),
-                         .flp_b(term3),
-                         .sign(Xn[31]),
-                         .exponent(Xn[30:23]),
-                         .prod(Xn[22:0]));                
+module Xncalculation (
+    input [15:0] D,
+    input [15:0] X,
+    output reg [15:0] Xn
+);
+
+localparam [15:0] two = 16'b0100000000000000; // value of 2
+
+reg [15:0] term1;
+reg [15:0] term2;
+reg [15:0] term3;
+reg [15:0] x1;
+
+// Calculate term1: D * X
+mul mul1(
+    .flp_a(D),
+    .flp_b(X),
+    .sign(term1[15]),
+    .exponent(term1[14:11]),
+    .prod(term1[10:0])
+);
+
+// Calculate term2: 2 - term1
+assign term2 = two - term1;
+
+// Calculate term3: X * term2
+mul mul2(
+    .flp_a(X),
+    .flp_b(term2),
+    .sign(Xn[15]),
+    .exponent(Xn[14:11]),
+    .prod(Xn[10:0])
+);
+
 endmodule
