3800402dbf65cc75314c71bdf9f7f8d8f2367230
Soumya Kulhalli <83526493+Soumya2754@users.noreply.github.com>
2024-05-20 12:45:52 +0530

    Update Softmax_Layer.v

--- SoftMax Layer/Softmax_Layer.v
+++ SoftMax Layer/Softmax_Layer.v
@@ -1,85 +1,83 @@
-
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-//By: Basant Loay Abdalla
-//////////////////////////////////////////////////////////////////////////////////
 
-module Softmax_Layer(inputData,outData,chosenProbability);
-localparam sizeArr=10;
-localparam width=32;
-input [319:0] inputData;
-output [319:0] outData;
-output [31:0] chosenProbability;
-wire [319:0] exponentialArr1;
-wire [319:0] expoenetialArr2;
-wire [319:0] expoenetialArr3;
-//wire [319:0] total;
-wire [31:0] total1;
-wire [319:0] max1;
-wire[319:0] recp;
-localparam one=32'b00111111100000000000000000000000;
-genvar i;
-//calculate exponential for input array
-for (i=0;i<sizeArr;i=i+1)
-begin
-exponential E(.inputX(inputData[i*width+:width]),
-              .temp(exponentialArr1[i*width+:width]));
-assign expoenetialArr2[i*width+:width] = exponentialArr1[i*width+:width];             
-end
-
-//calculate sum of 10 values 
-Accumulator1 
-#(
-.size_Of_Array(sizeArr),
-.DATA_WIDTH(width)
-)
-exponentialAddition(
-.Input_Arr(exponentialArr1),
-.Output(total1[31:0])
+module Softmax_Layer (
+    input [1023:0][15:0] inputData, // Array of 1024 elements, each 16 bits
+    output [1023:0][15:0] outData,  // Array of 1024 elements, each 16 bits
+    output [15:0] chosenProbability
 );
-// total1=total;
-/*for (i=0;i<sizeArr-1;i=i+1)
-begin
-assign total[(i+1)*width+:width]= total[i*width+:width];
-end 
-*/
-reciporical r1(.numerator(one),
-                 .divisor(total1),
-                 .reciporical1(recp[31:0]));
-for (i=0;i<sizeArr-1;i=i+1)
-                 begin
-                 assign recp[(i+1)*width+:width]= recp[i*width+:width];
-                 end                  
-//apply division module to calculate probability in array
 
-for (i=0;i<sizeArr;i=i+1)
-begin
-mul div(.flp_a(expoenetialArr2[i*width+:width]),
-         .flp_b(recp[i*width+:width]),
-         .sign(outData[width*(i)+31]),
-         .exponent(outData[(width*i+30) -:8]),
-         .prod(outData[(width*i+22)-:23]));
-            
-assign expoenetialArr3[i*width+:width] = outData[i*width+:width];                        
-end
+localparam sizeArr = 1024;
+localparam width = 16;
+localparam one = 16'b0011110000000000; // Example value for "one" in 16-bit float representation
 
-/*for (i=0;i<sizeArr;i=i+1)
-begin
-division div(.dividend(expoenetialArr2[i*width+:width]),
-             .divsor(total[i*width+:width]),
-             .outDiv(outData[i*width+:width])); 
-assign expoenetialArr3[i*width+:width] = outData[i*width+:width];                        
-end
-*/
+wire [1023:0][15:0] exponentialArr1;
+wire [15:0] total1;
+wire [15:0] recp;
+wire [1023:0][15:0] exponentialArr2;
+wire [1023:0][15:0] exponentialArr3;
+wire [1023:0][15:0] max1;
 
-//calculate maximum value in array
-assign max1[31:0]= expoenetialArr3[31:0];
-for (i=1;i<sizeArr;i=i+1)
-begin
-maxValue max1V(.in1(max1[(i-1)*width+:width]),
-              .in2(expoenetialArr3[i*width+:width]),
-              .outmax(max1[i*width+:width]));
-end
-assign chosenProbability = max1[319:288];
+genvar i;
+
+// Calculate exponential for each input array element
+generate
+    for (i = 0; i < sizeArr; i = i + 1) begin
+        exponential E (
+            .inputX(inputData[i]),
+            .outputY(exponentialArr1[i])
+        );
+        assign exponentialArr2[i] = exponentialArr1[i];
+    end
+endgenerate
+
+// Calculate sum of exponentials
+Accumulator1 #(
+    .size_Of_Array(sizeArr),
+    .DATA_WIDTH(width)
+) exponentialAddition (
+    .Input_Arr(exponentialArr1),
+    .Output(total1)
+);
+
+// Calculate reciprocal of the sum of exponentials
+reciporical r1 (
+    .numerator(one),
+    .divisor(total1),
+    .reciporical1(recp)
+);
+
+// Distribute the reciprocal value across the array
+generate
+    for (i = 1; i < sizeArr; i = i + 1) begin
+        assign recp[i+1] = recp[i];
+    end
+endgenerate
+
+// Normalize exponentials to get softmax probabilities
+generate
+    for (i = 0; i < sizeArr; i = i + 1) begin
+        mul div (
+            .flp_a(exponentialArr2[i]),
+            .flp_b(recp[i]),
+            .sign(outData[i][width-1]),
+            .exponent(outData[i][width-2+:7]),
+            .prod(outData[i][width-9 -:23])
+        );
+        assign exponentialArr3[i] = outData[i];
+    end
+endgenerate
+
+// Calculate maximum value in the array
+assign max1[0] = exponentialArr3[0];
+generate
+    for (i = 1; i < sizeArr; i = i + 1) begin
+        maxValue max1V (
+            .in1(max1[i-1]),
+            .in2(exponentialArr3[i]),
+            .outmax(max1[i])
+        );
+    end
+endgenerate
+assign chosenProbability = max1[sizeArr-1];
 
 endmodule
