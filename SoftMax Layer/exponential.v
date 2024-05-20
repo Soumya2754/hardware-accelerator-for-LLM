1f5ed412b067830bfe000081f81d94895a63e91d
Asavari Hangal <165581544+Emit-009@users.noreply.github.com>
2024-05-20 23:24:27 +0530

    Update exponential.v

--- SoftMax Layer/exponential.v
+++ SoftMax Layer/exponential.v
@@ -16,7 +16,7 @@ localparam [15:0] constants[5:0]={  16'b0000101100000111, // IEEE representation
                                                    
 input [15:0] inputX;
 wire  [95:0] outY;
-output wire [15:0] temp;
+output [15:0] temp;
 reg[15:0] exception=16'b0000111110000000;
 wire [95:0] powerX ;
 wire [95:0] holder;
@@ -28,19 +28,18 @@ assign powerX[15:0] =16'b0000111110000000;//initialzed by 1
 //calculating first six terms as first of value 1 initialized in outY
 // module term multiply input number consequetively and multiply by constant 
 genvar z;
-for (z = 0; z < noOfTerms; z = z + 1)
+for (z = 1; z < noOfTerms; z = z + 1)
 begin
     term firstTerm(.originalNum(inputX),
-                   .poweredNum(powerX[z*size +: size]),
+                   .poweredNum(powerX[(z+1)*size-1 : (z)*size]),
                    .reciporical(constants[z]),
-                   .outPower(powerX[(z+1)*size +: size]),
-                   .outTerm(holder[z*size +: size]));
+                   .outPower(powerX[(z+1)*size-1 : (z)*size]),
+                   .outTerm(holder[(z+1)*size-1 : (z)*size]));
                    
-    add addition1(.A_FP(holder[z*size +: size]),
-                  .B_FP(outY[z*size +: size]),
-                  .out(outY[(z+1)*size +: size]));
+    add addition1(.A_FP(holder[(z+1)*size-1 : (z)*size]),
+                  .B_FP(outY[(z+1)*size-1 : (z)*size]),
+                  .out(outY[(z+1)*size-1 : (z)*size]));
 end            
 
 assign temp = outY[95:80];
 endmodule
-
