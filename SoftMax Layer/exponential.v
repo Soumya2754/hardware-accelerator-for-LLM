a20eef567059bacc82fcda152a6bcfe67bf6513d
Asavari Hangal <165581544+Emit-009@users.noreply.github.com>
2024-05-20 15:04:21 +0530

    exponential.v

--- SoftMax Layer/exponential.v
+++ SoftMax Layer/exponential.v
@@ -2,22 +2,45 @@
 //////////////////////////////////////////////////////////////////////////////////
 //By: Basant Loay Abdalla
 //////////////////////////////////////////////////////////////////////////////////
-// The division module based on Newton Raphson method to calculate reciporical of divisor
-// then multiply reciporical by Dividend
-module division(dividend,divsor,outDiv);
-localparam [31:0] one=32'b00111111100000000000000000000000;
-input  [31:0] dividend ;
-input  [31:0] divsor;
-output [31:0] outDiv;
-wire [31:0] reciporical2;
 
-reciporical r1(.numerator(one),
-               .divisor(divsor),
-               .reciporical1(reciporical2)); 
-                                                    
-mul mulReciporical(.flp_a(dividend),
-                   .flp_b(reciporical2),
-                   .sign(outDiv[31]),
-                   .exponent(outDiv[30:23]),
-                   .prod(outDiv[22:0]));
+//calculating exponential using Taylor Expansion
+module exponential(inputX,temp);
+localparam size=16;
+localparam noOfTerms=6;
+localparam [15:0] constants[5:0]={  16'b0000101100000111, // IEEE representation of 1/720
+                                    16'b0000110000010001, // IEEE representation of 1/120
+                                    16'b0000110101010111, // IEEE representation of 1/24
+                                    16'b0000111000010111, // IEEE representation of 1/6
+                                    16'b0000111100000000, // IEEE representation of 1/2
+                                    16'b0000111110000000}; // IEEE representation of 1  
+                                                   
+input [15:0] inputX;
+wire  [95:0] outY;
+output wire [15:0] temp;
+reg[15:0] exception=16'b0000111110000000;
+wire [95:0] powerX ;
+wire [95:0] holder;
+reg [15:0] sum=16'b0000000000000000;//initialized by 0
+
+assign outY[15:0] =16'b0000111110000000;//initialzed by 1
+assign powerX[15:0] =16'b0000111110000000;//initialzed by 1
+
+//calculating first six terms as first of value 1 initialized in outY
+// module term multiply input number consequetively and multiply by constant 
+genvar z;
+for (z = 0; z < noOfTerms; z = z + 1)
+begin
+    term firstTerm(.originalNum(inputX),
+                   .poweredNum(powerX[z*size +: size]),
+                   .reciporical(constants[z]),
+                   .outPower(powerX[(z+1)*size +: size]),
+                   .outTerm(holder[z*size +: size]));
+                   
+    add addition1(.A_FP(holder[z*size +: size]),
+                  .B_FP(outY[z*size +: size]),
+                  .out(outY[(z+1)*size +: size]));
+end            
+
+assign temp = outY[95:80];
 endmodule
+
